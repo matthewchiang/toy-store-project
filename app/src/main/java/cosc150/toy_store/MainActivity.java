ef90472f80b737af27b18e94c54054105990e996
Matthew Chiang <matthewchiang18@gmail.com>
2016-04-05 18:34:11 -0400

    First commit!

---
+++ app/src/main/java/cosc150/toy_store/MainActivity.java
@@ -0,0 +1,13 @@
+package cosc150.toy_store;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
